25e9bf8433f7e2afb52dd9718ba03f9d4e343456
JI-EUN <pangdoa_merry@daum.net>
2015-10-26 18:25:45 +0900

    2015.10.26

---
+++ 시험문제/js/script.js
@@ -0,0 +1,113 @@
+$(function(){
+
+	$('.content>section:odd').css('background-color','#f3f3f3');
+
+	/*아래 부터 스크립트시작*/
+	//1번문제
+	$('header ul li').mouseenter(function(){
+		$(this).css('background','#677b8a').find('a').css('color','#fff').parent().siblings().css('background','#CCCCCC').find('a').css('color','#000')
+	}).mouseleave(function(){
+		$(this).css('background','#CCCCCC').find('a').css('color','#000')
+	})
+
+	//2번문제
+
+	//3번문제(slide)
+	 var bbb=$('.slidebanner ul li')
+	 var idx=Math.floor(Math.random()*bbb.size())
+	 bbb.eq(idx).addClass('on').find('img').css('display','block')
+	 function movement(st,end,index){
+			  $('.slidebanner ul li').eq(index).addClass('on').find('img').css({
+				  'display':'block',
+				  'left':st
+			  }).stop().animate({
+				  'left':end
+			  }).parent().siblings().removeClass('on')
+			  if(index==$('.slidebanner ul li').size()){
+				  idx=0
+				  movement('100%','0',idx)
+			  }else if(index<0){
+				  idx= $('.slidebanner ul li').size()-1
+				  movement('-100%','0',idx)
+			  }
+	 }
+	 $('.slidebanner .next').click(function(){
+		 movement('0','-100%',idx)
+		 idx++
+		 movement('100%',0,idx)
+	 })
+	 $('.slidebanner .prev').click(function(){
+		 movement('0','100%',idx)
+		 idx--
+		 movement('-100%',0,idx)
+	 })
+
+	$('.d-type .slidebanner li a').click(function(){
+		idx=$('.slidebanner li.on').index()
+		var aaa=$(this).parent().index()
+
+ 		if (idx<aaa) {
+ 			movement(0,'-100%',idx)
+			movement('100%',0,aaa)
+ 		}else if (idx>aaa) {
+ 			movement(0,'100%',idx)
+			movement('-100%',0,aaa)
+ 		}else {
+
+ 		}
+		return false
+	})
+	var slidemove=setInterval(function(){
+		$('.slidebanner .prev').click()
+	},1000)
+	$('.slidebanner ul').on({
+		mouseenter:function(){
+			clearInterval(slidemove)
+		},mouseleave:function(){
+			slidemove=setInterval(function(){
+			   $('.slidebanner .prev').click()
+		   },1000)
+		}
+	})
+	//4번문제(fade)
+	var fadeidx=0
+	function fadeinout(){
+		$('.fadebanner li').eq(fadeidx).addClass('on').find('img').fadeIn().parent().siblings().removeClass('on').find('img').fadeOut()
+		fadeidx++
+		if(fadeidx==$('.fadebanner li').size()){
+			fadeidx=0
+		}
+	}
+	var auto=setInterval(function(){fadeinout('next')},1000)
+	$('.fadebanner ul').on({
+		mouseenter:function(){
+			clearInterval(auto)
+		},mouseleave:function(){
+			auto=setInterval(function(){fadeinout('next')},1000)
+		}
+	})
+	$('.fadebanner li a').click(function(){
+		fadeidx=$(this).parent().index()
+		$('.fadebanner li').eq(fadeidx).addClass('on').find('img').fadeIn().parent().siblings().removeClass('on').find('img').fadeOut()
+		return false
+	})
+
+
+	//5번문제(youtube)
+
+	$('.f-type ul li').click(function(){
+		$(this).css('opacity','0.5').siblings().css('opacity','1')
+		var movlist=$(this).children('a').attr('href')
+		var movurl="https://www.youtube.com/embed/"+movlist+"?rel=0&amp;controls=0&amp;showinfo=0"
+		$('.f-type .movie-view iframe').attr('src',movurl)
+		return false
+	})
+
+	//6번문제(wingbanner)
+
+	function wingbanner(){
+		var wingLeft=$('.content').offset().left
+		console.log(wingLeft)
+	}
+
+})
